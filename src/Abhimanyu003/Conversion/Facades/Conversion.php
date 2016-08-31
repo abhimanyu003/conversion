21ab5cfa431723ce015b1a0c9e66a92c49a32405
Abhimanyu Sharma <abhimanyusharma003@gmail.com>
2016-09-01 01:24:38 +0530

    Added missing facade file

---
+++ src/Abhimanyu003/Conversion/Facades/Conversion.php
@@ -0,0 +1,17 @@
+<?php namespace Abhimanyu003\Conversion\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Conversion extends Facade
+{
+
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'conversion';
+    }
+}
